b7e7f741ae74416c861f80b4e1787a5265370b07
Kiet Hoang <kiethoang101@gmail.com>
2022-08-16 15:50:55 +0700

    feat: 🎸 Fix bug Header

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,8 @@ import routesConfig from '~/config/routes';
 import { HeaderOnly } from '~/components/Layout';
 
 // Pages
-import Home from '~/pages/Following';
-import Following from '~/pages/Home';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
